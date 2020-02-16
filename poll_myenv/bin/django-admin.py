0b5d17f5d45dd4189054520111380759beb93e71
Ashdeep <ashdeepupadhyay@gmail.com>
2020-02-16 12:21:30 +0530

    basic setup of the project

---
+++ poll_myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ashdeep/ashdeeplearn/projects/python_django/poll_app_in_django/poll_myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
