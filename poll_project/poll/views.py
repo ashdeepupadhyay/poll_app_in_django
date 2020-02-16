8267573f36f08fce4751a8347d46a795d7b13559
Ashdeep <ashdeepupadhyay@gmail.com>
2020-02-16 13:00:05 +0530

    rendering the views and making the URL's Work

--- poll_project/poll/views.py
+++ poll_project/poll/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context={}
+    return render(request,"polls/home.html",context)
+
+def create(request):
+    context={}
+    return render(request,"polls/create.html",context)
+
+def results(request,poll_id):
+    context={}
+    return render(request,"polls/results.html",context)
+
+def vote(request,poll_id):
+    context={}
+    return render(request,"polls/vote.html",context)
